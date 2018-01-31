e2612e5deb45b897912498d25ab8650181af191f
slayerSahara <sjones@student.neumont.edu>
2018-01-31 12:12:56 -0700

    deleted things

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 var Users = mongoose.model('userInfo');
 mongoose.connect('mongodb://sjones:Rumple!1630@ds249737.mlab.com:49737/mom_hotline');
 
-app.use(express.static('C:/Users/Sarah/Desktop/Capstone/Capstone-Mom/MOM' + '/public'));
+app.use(express.static('C:/Users/Sarah/Desktop/Capstone/Capstone-Mom/MOM' + '/../public'));
 app.set('views', path.join(__dirname + '/views'));
 app.engine('.html', require('ejs').__express);
 app.set('view engine', 'html');
@@ -22,44 +22,9 @@ app.get('/', function(req, res){
     res.render('chat')
 });
 
-// app.all('*', function(req, res, next){
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-//     res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
-
-//     if(req.method == 'OPTIONS'){
-//         res.status(200).end();
-//     }
-//     else {
-//         next();
-//     }
-// });
-
 app.get('/', function(req, res){
     res.sendFile('chat')
 });
-
-// app.post('/setup', function(req, res){
-//     var chatData = [{
-//         created: new Date(),
-//         content: 'Hi',
-//         username: 'Sahara'
-//     }, {
-//         created: new Date(),
-//         content: 'You suck',
-//         username: 'Bob'
-//     }];
-
-//     for(var c = 0; c < chatData.length; c++){
-//         var newChat = new Chat(chatData[c]);
-        
-//         newChat.save(function(err, savedChat){
-//             console.log(savedChat);
-//         });
-//     }
-
-//     res.send('created');
-// });
 
 var PORT = process.env.PORT || 3000;
 
