db8a936bda3fe546d395de3a640fcab4753ddcb1
slayerSahara <sjones@student.neumont.edu>
2018-01-31 09:44:43 -0700

    My bad...

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ var app = express();
 var Users = mongoose.model('userInfo');
 mongoose.connect('mongodb://sjones:Rumple!1630@ds249737.mlab.com:49737/mom_hotline');
 
+app.use(express.static('C:/Users/Sarah/Desktop/Capstone/Mom' + '/public'));
+
 new Users({
     username: 'bob',
     password: '125720taco'
@@ -14,6 +16,45 @@ new Users({
 
 app.get('/', function(req, res){
     res.sendFile('C:/Users/Sarah/Desktop/Capstone/Mom/views/chat.html')
+});
+
+app.all('*', function(req, res, next){
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
+
+    if(req.method == 'OPTIONS'){
+        res.status(200).end();
+    }
+    else {
+        next();
+    }
+});
+
+app.get('/', function(req, res){
+    res.sendFile('chat.html')
+});
+
+app.post('/setup', function(req, res){
+    var chatData = [{
+        created: new Date(),
+        content: 'Hi',
+        username: 'Sahara'
+    }, {
+        created: new Date(),
+        content: 'You suck',
+        username: 'Bob'
+    }];
+
+    for(var c = 0; c < chatData.length; c++){
+        var newChat = new Chat(chatData[c]);
+        
+        newChat.save(function(err, savedChat){
+            console.log(savedChat);
+        });
+    }
+
+    res.send('created');
 });
 
 var PORT = process.env.PORT || 3000;
--- javascript/server.js
+++ javascript/server.js
@@ -4,8 +4,6 @@ var io = require('socket.io')(server);
 var app = express();
 var mongoose = require('mongoose');
 
-app.use(express.static('C:/Users/Sarah/Desktop/Capstone/Mom' + '/public'));
-
 mongoose.connect('mongodb://sjones:Rumple!1630@ds249737.mlab.com:49737/mom_hotline');
 
 var chatSchema = mongoose.Schema({
@@ -17,44 +15,6 @@ var chatSchema = mongoose.Schema({
 var chat = mongoose.model('Chat', chatSchema);
 
 // allow cross-origin resource sharing (allows resourdces to be requested from another domain)
-app.all('*', function(req, res, next){
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
-
-    if(req.method == 'OPTIONS'){
-        res.status(200).end();
-    }
-    else {
-        next();
-    }
-});
-
-app.get('/', function(req, res){
-    res.sendFile('chat.html')
-});
-
-app.post('/setup', function(req, res){
-    var chatData = [{
-        created: new Date(),
-        content: 'Hi',
-        username: 'Sahara'
-    }, {
-        created: new Date(),
-        content: 'You suck',
-        username: 'Bob'
-    }];
-
-    for(var c = 0; c < chatData.length; c++){
-        var newChat = new Chat(chatData[c]);
-        
-        newChat.save(function(err, savedChat){
-            console.log(savedChat);
-        });
-    }
-
-    res.send('created');
-});
 
 io.on('connection', function(socket){
     var defaultRoom = 'general';
