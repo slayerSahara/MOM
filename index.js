006f9eb0b6c3b5936e7cd96de91fb9e373668a0a
slayerSahara <sjones@student.neumont.edu>
2018-01-31 10:55:37 -0700

    Commenting out some stuff

--- index.js
+++ index.js
@@ -13,53 +13,53 @@ app.set('views', path.join(__dirname + '/views'));
 app.engine('.html', require('ejs').__express);
 app.set('view engine', 'html');
 
-new Users({
-    username: 'bob',
-    password: '125720taco'
-}).save();
+// new Users({
+//     username: 'bob',
+//     password: '125720taco'
+// }).save();
 
 app.get('/', function(req, res){
     res.render('chat')
 });
 
-app.all('*', function(req, res, next){
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
+// app.all('*', function(req, res, next){
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//     res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
 
-    if(req.method == 'OPTIONS'){
-        res.status(200).end();
-    }
-    else {
-        next();
-    }
-});
+//     if(req.method == 'OPTIONS'){
+//         res.status(200).end();
+//     }
+//     else {
+//         next();
+//     }
+// });
 
 app.get('/', function(req, res){
     res.sendFile('chat')
 });
 
-app.post('/setup', function(req, res){
-    var chatData = [{
-        created: new Date(),
-        content: 'Hi',
-        username: 'Sahara'
-    }, {
-        created: new Date(),
-        content: 'You suck',
-        username: 'Bob'
-    }];
+// app.post('/setup', function(req, res){
+//     var chatData = [{
+//         created: new Date(),
+//         content: 'Hi',
+//         username: 'Sahara'
+//     }, {
+//         created: new Date(),
+//         content: 'You suck',
+//         username: 'Bob'
+//     }];
 
-    for(var c = 0; c < chatData.length; c++){
-        var newChat = new Chat(chatData[c]);
+//     for(var c = 0; c < chatData.length; c++){
+//         var newChat = new Chat(chatData[c]);
         
-        newChat.save(function(err, savedChat){
-            console.log(savedChat);
-        });
-    }
+//         newChat.save(function(err, savedChat){
+//             console.log(savedChat);
+//         });
+//     }
 
-    res.send('created');
-});
+//     res.send('created');
+// });
 
 var PORT = process.env.PORT || 3000;
 
