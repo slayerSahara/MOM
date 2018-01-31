69aaae833a5575b2dcd180e756ae60c881d52b48
slayerSahara <sjones@student.neumont.edu>
2018-01-31 09:37:26 -0700

    For the love of GOD

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
-var app = require('express');
+var express = require('express');
 require('./models/users');
+
+var app = express();
 
 var Users = mongoose.model('userInfo');
 mongoose.connect('mongodb://sjones:Rumple!1630@ds249737.mlab.com:49737/mom_hotline');
@@ -9,3 +11,7 @@ new Users({
     username: 'bob',
     password: '125720taco'
 }).save();
+
+var PORT = process.env.PORT || 3000;
+
+app.listen(PORT);
