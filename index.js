1ad6add21913b43784fe9c7c667a9cf4b6a50edc
slayerSahara <sjones@student.neumont.edu>
2018-02-06 22:16:31 -0700

    Lots of changes and stuff

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 var mongoose = require('mongoose');
 var express = require('express');
-var path = require('path');
-require('./models/users');
-
 var app = express();
+var http = require('http'); 
+var io = require('socket.io')(http);
+var path = require('path');
+var PORT = process.env.PORT || 3000;
+
+require('./models/users');
 
 var Users = mongoose.model('userInfo');
 mongoose.connect('mongodb://sjones:Rumple!1630@ds249737.mlab.com:49737/mom_hotline');
 
-app.use(express.static('C:/Users/Sarah/Desktop/Capstone/Capstone-Mom/MOM' + '/../public'));
+app.use(express.static(path.join(__dirname + '/public')));
 app.set('views', path.join(__dirname + '/views'));
 app.engine('.html', require('ejs').__express);
 app.set('view engine', 'html');
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/chat.html');
+});
+
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
+    });
+});
 
 // new Users({
 //     username: 'bob',
@@ -27,7 +40,7 @@ app.get('/about', function(req, res){
 app.get('/contact', function(req, res){
     res.render('contact')
 });
-app.get('/index', function(req, res){
+app.get('/index' || '/', function(req, res){
     res.render('index')
 });
 app.get('/find_help', function(req, res){
@@ -45,7 +58,5 @@ app.get('/prof_1', function(req, res){
 app.get('/prof_2', function(req, res){
     res.sender('prof_2')
 });
-
-var PORT = process.env.PORT || 3000;
 
 app.listen(PORT);
